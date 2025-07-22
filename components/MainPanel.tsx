9023d92aa2b5dc871aa29f2dc78a828dc41ac145
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-07-22 11:25:22 -0300

    implemented websocket initial handling

--- components/MainPanel.tsx
+++ components/MainPanel.tsx
@@ -1,12 +1,40 @@
-import { WebSocketManager } from "@/utils/WebSocketManager"
-import { useEffect } from "react"
+"use client"
+import { BroadcastDTO } from "@/structures/BroadcastDTO"
+import { useEffect, useState } from "react"
 
 export default function(){
-    const webSocketManager = new WebSocketManager()
     
-    return (
-        <>
+    const [broadcastObject, updateBroadcastObject] = useState<BroadcastDTO>({
+            status: '',
+            macros_running: [{
+                time_started: '',
+                name: ''
+            }]
+    })
+    useEffect(() =>{
+        const ws_url = String(process.env["NEXT_PUBLIC_WS_URL"])
+        const socket = new WebSocket(ws_url)
+
+        socket.onmessage = (event) => {
+            var json_content = JSON.parse(event.data)
+            console.log(broadcastObject)
+            updateBroadcastObject(json_content)
             
-        </>
-    )
+        };
+    }, [])
+    
+    
+    
+    useEffect(() =>{
+        console.log("it changed")
+    }, [broadcastObject])
+
+    if(broadcastObject){
+        return (
+            <div>
+                {broadcastObject?.status}        
+            </div>
+        )
+    }
+    
 }
--- structures/BroadcastDTO.ts
+++ structures/BroadcastDTO.ts
@@ -2,6 +2,5 @@ import { Macro } from "./Macro";
 
 export interface BroadcastDTO{
     status: string;
-    time_started: string;
     macros_running: [Macro]
 }
